9b9d908ab61481c09e41e6c1520574fe0584fa75
chaijewon <vcandjava@nate.com>
2025-06-17 09:37:47 +0900

    1111

--- src/main/java/com/sist/web/Devops3Application.java
+++ src/main/java/com/sist/web/Devops3Application.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Devops3Application {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Devops3Application.class, args);
-	}
-
+	}    
 }
+
